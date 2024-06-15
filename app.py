91f2378fcc1354193aafd8a9fe2d80ae4e6832fd
code5678 <152993219+code5678@users.noreply.github.com>
2024-06-15 18:29:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 @st.cache_data
 def load_data():
     df = pd.read_csv('final.csv')
-    df['Opening Rank'] = pd.to_numeric(df['Opening Rank'], errors='coerce')
+    df['Closing Rank'] = pd.to_numeric(df['Closing Rank'], errors='coerce')
     return df
 
 def filter_and_plot(institute, seat_type, gender, quota):
@@ -31,56 +31,52 @@ def filter_and_plot(institute, seat_type, gender, quota):
         for year, last_round in last_rounds.items()
     ])
 
-    filtered_data = filtered_data.dropna(subset=['Opening Rank'])
+    filtered_data = filtered_data.dropna(subset=['Closing Rank'])
     filtered_data = filtered_data.drop_duplicates(subset=['Year', 'Branch'])
-    pivot_df = filtered_data.pivot(index='Year', columns='Branch', values='Opening Rank')
+    pivot_df = filtered_data.pivot(index='Year', columns='Branch', values='Closing Rank')
 
     plt.figure(figsize=(14, 10))
 
     for branch in pivot_df.columns:
         plt.plot(pivot_df.index, pivot_df[branch], marker='o', linestyle='-', label=branch)
 
-    plt.title(f'Opening Ranks for All Branches in {institute} ({seat_type}, {gender}, {quota}) (2016-2023)')
+    plt.title(f'Closing Ranks for All Branches in {institute} ({seat_type}, {gender}, {quota}) (2016-2023)')
     plt.xlabel('Year')
-    plt.ylabel('Opening Rank')
+    plt.ylabel('Closing Rank')
     plt.legend(title='Branch', bbox_to_anchor=(0.5, -0.15), loc='upper center', ncol=3)
     plt.grid(True)
     plt.tight_layout()
     st.pyplot(plt)
 
 def main():
-    st.title("IIIT Opening Ranks Over the Years")
+    st.title("IIT Closing Ranks Over the Years")
 
     institutes = [
-     
-"Atal Bihari Vajpayee Indian Institute of Information Technology & Management Gwalior",
-"Indian Institute of Information Technology (IIIT)Kota, Rajasthan",
-"Indian Institute of Information Technology Guwahati",
-"Indian Institute of Information Technology(IIIT) Kalyani, West Bengal",
-"Indian Institute of Information Technology(IIIT) Kilohrad, Sonepat, Haryana",
-"Indian Institute of Information Technology(IIIT) Una, Himachal Pradesh",
-"Indian Institute of Information Technology (IIIT), Sri City, Chittoor",
-"Indian Institute of Information Technology(IIIT), Vadodara, Gujrat",
-"Indian Institute of Information Technology, Allahabad",
-"Indian Institute of Information Technology, Design & Manufacturing, Kancheepuram",
-"Pt. Dwarka Prasad Mishra Indian Institute of Information Technology, Design & Manufacture Jabalpur",
-"Indian Institute of Information Technology  Manipur",
-"Indian Institute of Information Technology Tiruchirappalli",
-"Indian Institute of Information Technology Lucknow",
-"Indian Institute of Information Technology(IIIT) Dharwad",
-"Indian Institute of Information Technology Design & Manufacturing Kurnool, Andhra Pradesh",
-"Indian Institute of Information Technology(IIIT) Kottayam",
-"Indian Institute of Information Technology (IIIT) Ranchi",
-"Indian Institute of Information Technology (IIIT) Nagpur",
-"Indian Institute of Information Technology (IIIT) Pune",
-"Indian Institute of Information Technology Bhagalpur",
-"Indian Institute of Information Technology Bhopal",
-"Indian Institute of Information Technology Surat",
-"Indian Institute of Information Technology, Agartala",
-"Indian Institute of Information Technology, Vadodara International Campus Diu (IIITVICD)",
-"Indian Institute of Information Technology(IIIT), Sri City, Chittoor District, Andra Pradesh",
-"Indian Institute of Information Technology Srirangam, Tiruchirappalli",
-"Indian Institute of Information Technology(IIIT), Sri City, Chittoor District, Andhra Pradesh"
+        "Indian Institute  of Technology Bhubaneswar",
+        "Indian Institute  of Technology Bombay",
+        "Indian Institute  of Technology Guwahati",
+        "Indian Institute of Technology Bhilai",
+        "Indian Institute of Technology Goa",
+        "Indian Institute of Technology Jammu",
+        "Indian Institute of Technology Dharwad",
+        "Indian Institute  of Technology Mandi",
+        "Indian Institute  of Technology Delhi",
+        "Indian Institute  of Technology Indore",
+        "Indian Institute  of Technology Kharagpur",
+        "Indian Institute  of Technology Hyderabad",
+        "Indian Institute  of Technology Jodhpur",
+        "Indian Institute  of Technology Kanpur",
+        "Indian Institute  of Technology Madras",
+        "Indian Institute  of Technology Gandhinagar",
+        "Indian Institute  of Technology Patna",
+        "Indian Institute  of Technology Roorkee",
+        "Indian Institute  of Technology (ISM) Dhanbad",
+        "Indian Institute  of Technology Ropar",
+        "Indian Institute  of Technology (BHU) Varanasi",
+        "Indian Institute  of Technology Palakkad",
+        "Indian Institute  of Technology Tirupati"
+        
+        # Add other IITs as necessary
     ]
     seat_types = ['OPEN', 'EWS', 'OBC-NCL', 'SC', 'ST', 'OPEN (PwD)', 'EWS (PwD)', 'OBC-NCL (PwD)', 'SC (PwD)', 'ST (PwD)']
     genders = ['Gender-Neutral', 'Female-only (including Supernumerary)']
